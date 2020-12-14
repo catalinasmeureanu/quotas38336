59e05c62dcffbaf7658301f68a8b3cf88023e8f0
Catalina <51541330+catalinasmeureanu@users.noreply.github.com>
2020-12-14 16:29:50 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
+provider "vault" {
+  address = "http://127.0.0.1:8200"
+}
+
+resource "vault_namespace" "dev" {
+  path = "dev"
+}
+
+resource "vault_quota_rate_limit" "dev" {
+  name = "quota-dev"
+  path = "${vault_namespace.dev.path}/"
+  rate = 50
+}
